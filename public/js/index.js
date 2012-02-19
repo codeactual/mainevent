653b940764011bddf4188fe5ec0b794af7f4e35b
David Smith <codeactual@gmail.com>
2012-02-19 07:39:14 +0000

    Refactor and document.

--- public/js/index.js
+++ public/js/index.js
@@ -15,8 +15,8 @@ $(function(){
     },
 
     timeline: function(options) {
-      var o = {
-        filters: {},
+      var search_args = {
+        conditions: {},
         skip: null,
         limit: null,
         sort_attr: null,
@@ -24,43 +24,41 @@ $(function(){
       };
 
       if (undefined !== options) {
+        // Ex. '/#timeline/limit=10;skip=20;host=127.0.0.1'.
         var parts = options.split(';');
         _.each(parts, function(part) {
-          console.log('part', part);
-          var kv = part.split('=');
-          if (2 == kv.length) {
-            if (o.hasOwnProperty(kv[0])) {
-              o[kv[0]] = kv[1];
+          // Ex. 'limit=10'. Push key/value pairs into search_args.
+          var assign_part = part.split('=');
+          if (2 == assign_part.length) {
+            if (_.has(search_args, assign_part[0])) {
+              search_args[assign_part[0]] = assign_part[1];
             } else {
-              o.filters[kv[0]] = kv[1];
+              search_args.conditions[assign_part[0]] = assign_part[1];
             }
           }
         });
       }
 
-      _.each(o.filters, function(value, key) {
-        o[key] = value;
+      // Move search_args.conditions properties into search_args.
+      _.each(search_args.conditions, function(value, key) {
+        search_args[key] = value;
       });
-      delete o.filters;
+      delete search_args.conditions;
 
-      this._timeline(_.filter(o, function(value) { return null !== value; }));
-    },
-
-    _timeline: function(options) {
-      var url = '/timeline?' + $.param(options);
-
-      window.Timeline = Backbone.Collection.extend({
-        model: Event,
-        url: url,
-        initialize: function() {
-          this.bind('add', function(model) {
-            console.log('Timeline->add()', model);
-          });
-          this.bind('remove', function(model) {
-            console.log('Timeline->remove()', model);
-          });
+      // Remove unused options.
+      _.each(search_args, function(value, key) {
+        if (null === value) {
+          delete search_args[key];
         }
       });
+      this.timeline_search(search_args);
+    },
+
+    timeline_search: function(options) {
+      window.Timeline = Backbone.Collection.extend({
+        url: '/timeline?' + $.param(options)
+      });
+
       window.TimelineEventView = Backbone.View.extend({
         initialize: function() {
           this.model.bind('change', this.render, this);
@@ -75,27 +73,27 @@ $(function(){
           );
         }
       });
+
       window.TimelinePageView = Backbone.View.extend({
         el: $('#backbone-view'),
         initialize: function() {
-          dust.render(
-            'timeline_table',
-            null,
-            function(err, out) {
-              $('#backbone-view').html(out);
-              var tl = new Timeline();
-              tl.fetch({
-                success: function(collection, response) {
-                  _.each(response, function(eventdata) {
-                    var newmodel = new Event(eventdata);
-                    tl.add(newmodel);
-                    var view = new TimelineEventView({model: newmodel});
-                    view.render();
-                  });
-                }
-              });
-            }
-          );
+          // Sync template with data fetched from server.
+          var on_template_rendered = function(err, out) {
+            $('#backbone-view').html(out);
+
+            var timeline = new Timeline();
+            timeline.fetch({
+              success: function(collection, response) {
+                _.each(response, function(event) {
+                  var model = new Event(event);
+                  timeline.add(model);
+                  var view = new TimelineEventView({ model: model });
+                  view.render();
+                });
+              }
+            });
+          };
+          dust.render('timeline_table', null, on_template_rendered);
         }
       });
       window.TimelinePage = new TimelinePageView();
@@ -106,7 +104,6 @@ $(function(){
       window.EventView = Backbone.View.extend({
         el: $('#backbone-view'),
         initialize: function() {
-          // Fires automatically during EventPageView.initialize.
           this.model = new Event({id: id});
           this.model.bind('change', this.render, this);
           this.model.set('parent', this.el);
