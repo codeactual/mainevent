44b3efd4ff2fde4f59eeaecec1a35ab0393c740a
David Smith <codeactual@gmail.com>
2012-03-18 05:26:39 +0000

    Wrap import.js in a function.

--- app/import.js
+++ app/import.js
@@ -6,47 +6,49 @@
 
 'use strict';
 
-var program = require('commander');
+(function() {
+  var program = require('commander');
 
-var splitCsv = function(list) {
-  return list.split(',');
-}
+  var splitCsv = function(list) {
+    return list.split(',');
+  }
 
-// Support all attributes normally defined by config.js.
-program
-  .option('-p, --parser <name>', 'Ex. nginx_access')
-  .option('-P, --path <file>', 'Ex. /var/log/nginx/access.log')
-  .option('-t, --tags [list]', 'Ex. tag1,tag2', splitCsv)
-  .option('-T, --timeAttr [name]', 'Ex. logtime')
-  .option('-r, --previewAttr [name]', 'Ex. message', splitCsv)
-  .parse(process.argv);
+  // Support all attributes normally defined by config.js.
+  program
+    .option('-p, --parser <name>', 'Ex. nginx_access')
+    .option('-P, --path <file>', 'Ex. /var/log/nginx/access.log')
+    .option('-t, --tags [list]', 'Ex. tag1,tag2', splitCsv)
+    .option('-T, --timeAttr [name]', 'Ex. logtime')
+    .option('-r, --previewAttr [name]', 'Ex. message', splitCsv)
+    .parse(process.argv);
 
-var source = {
-  parser: program.parser,
-  path: program.path,
-  tags: program.tags,
-  timeAttr: program.timeAttr,
-  previewAttr: program.previewAttr
-};
+  var source = {
+    parser: program.parser,
+    path: program.path,
+    tags: program.tags,
+    timeAttr: program.timeAttr,
+    previewAttr: program.previewAttr
+  };
 
-if (!source.parser || !source.path) {
-  console.error('--parser and --path are required');
-  process.exit(1);
-}
+  if (!source.parser || !source.path) {
+    console.error('--parser and --path are required');
+    process.exit(1);
+  }
 
-require(__dirname + '/modules/diana.js');
-var parser = diana.requireModule('parsers/parsers').createInstance(source.parser);
+  require(__dirname + '/modules/diana.js');
+  var parser = diana.requireModule('parsers/parsers').createInstance(source.parser);
 
-var lazy = require('lazy');
-new lazy(require("fs").createReadStream(source.path))
-  .lines
-  .map(String)
-  .filter(function(line) {
-    // lazy will convert a blank line to "undefined"
-    return line !== 'undefined';
-  })
-  .join(function (lines) {
-    if (lines.length) {
-      parser.parseAndInsert(source, lines);
-    }
-  });
+  var lazy = require('lazy');
+  new lazy(require("fs").createReadStream(source.path))
+    .lines
+    .map(String)
+    .filter(function(line) {
+      // lazy will convert a blank line to "undefined"
+      return line !== 'undefined';
+    })
+    .join(function (lines) {
+      if (lines.length) {
+        parser.parseAndInsert(source, lines);
+      }
+    });
+})();
