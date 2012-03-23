33538ce35a05d11fdd507a87102a2bbfb1ba3238
David Smith <codeactual@gmail.com>
2012-03-23 10:16:05 +0000

    Add job to count events by parser.

---
+++ app/jobs/count_by_parser.js
@@ -0,0 +1,30 @@
+/**
+ * Collect event count by parser.
+ */
+
+'use strict';
+
+require(__dirname + '/../modules/diana.js');
+var storage = diana.requireModule('storage/storage').createInstance();
+
+var options = {out: {replace : "top_parsers"}};
+
+var map = function() {
+  emit(this.parser, {count: 1});
+};
+
+var reduce = function(key, values) {
+  var result = {count: 0};
+  values.forEach(function(value) {
+    result.count += value.count;
+  });
+  return result;
+};
+
+storage.dbConnectAndOpen(function() {}, function(err, db) {
+  storage.dbCollection(db, storage.collection, function() {}, function(err, collection) {
+    collection.mapReduce(map, reduce, options, function(err, results, stats) {
+      storage.dbClose();
+    });
+  });
+});
