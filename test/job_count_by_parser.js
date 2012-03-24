d2980009249d7d3381e0529f091adfb0f099fd82
David Smith <codeactual@gmail.com>
2012-03-24 22:28:45 +0000

    Move time-ranged job verifier to new module.

--- test/job_count_by_parser.js
+++ test/job_count_by_parser.js
@@ -1,54 +1,31 @@
 'use strict';
 
 var testutil = require(__dirname + '/modules/testutil.js');
-var storage = diana.requireModule('storage/storage').createInstance();
-var parsers = diana.requireModule('parsers/parsers');
+var job = require(__dirname + '/modules/job.js');
 var jobName = 'count_by_parser';
-var job = diana.requireJob(jobName).run;
 var strtotime = diana.shared.Date.strtotime;
-
-/**
- * Verify the job results for a given interval and log set.
- *
- * @param run {String} Unique run ID.
- * @param test {Object} Test instance.
- * @param startTime {Number}
- * @param endTime {Number}
- * @param logs {Array} Event objects.
- * @param expected {Object} Reduce results indexed by their _id values.
- */
-var verifyWindow = function(test, run, startTime, endTime, logs, expected, callback) {
-  parsers.parseAndInsert(logs, function() {
-    var query = {message: run}; // Only for verification lookup.
-    job(startTime, endTime, query, function() {
-      storage.getMapReduceResults(jobName, function(err, docs) {
-        test.deepEqual(docs, expected);
-        test.done();
-      });
-    });
-  });
-};
 
 exports.testCountByParser = function(test) {
   test.expect(1);
-  var run = testutil.getRandHash();
+  var runId = testutil.getRandHash();
   var logs = [
     {
       source: {parser: 'json'},
-      lines: ['{"time":"3/12/2012 09:00:00","message":"' + run + '"}']
+      lines: ['{"time":"3/12/2012 09:00:00","message":"' + runId + '"}']
     },
     {
       source: {parser: 'php'},
       lines: [
-        '[12-Mar-2012 10:00:00 UTC] ' + run,
-        '[12-Mar-2012 11:00:00 UTC] ' + run
+        '[12-Mar-2012 10:00:00 UTC] ' + runId,
+        '[12-Mar-2012 11:00:00 UTC] ' + runId
       ]
     }
   ];
   var expected = {php: {count: 2}, json: {count: 1}};
-  verifyWindow(
+  job.verifyTimeRange(
     test,
-    run,
+    jobName,
+    runId,
     strtotime('3/12/2012 09:00:00'),
     strtotime('3/12/2012 12:00:00'),
     logs,
