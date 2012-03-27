8a2a7223ee8b438f601ea5c017931df6da10f0ec
David Smith <codeactual@gmail.com>
2012-03-27 00:46:50 +0000

    Add placeholder graph.

--- public/js/views/DashboardMainGraph.js
+++ public/js/views/DashboardMainGraph.js
@@ -14,6 +14,24 @@ define([], function() {
     },
 
     render: function() {
+      // Use convention-based IDs so markup can just hold positional containers.
+      var id = this.el.id + '-canvas';
+      this.$el.append($('<div>').attr('id', id));
+
+      $.jqplot(
+        id,
+        [
+          [[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]],
+          [[2, 4],[6,10.12],[10,26.1],[14,66.6],[18,170.9],[22,440.9]]
+        ],
+        {
+          title: 'Events',
+          series: [
+            {color:'#5FAB78'},
+            {color:'#000000'},
+          ]
+        }
+      );
     }
   });
 });
