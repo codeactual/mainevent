6220900ff8286515ec01efd254a353263a2bf36c
David Smith <codeactual@gmail.com>
2012-04-10 20:58:33 +0000

    Fix parser name.

--- test/fixtures/tail-config.js
+++ test/fixtures/tail-config.js
@@ -4,7 +4,7 @@ exports.read = function () {
   var config = mainevent.getConfig();
   config.sources = [{
     path: '/tmp/mainevent-test-tail.log',
-    parser: 'json',
+    parser: 'Json',
     tags: ['test1', 'test2'],
     timeAttr: 't',
     previewAttr: ['subject']
