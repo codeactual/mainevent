44bdf75f68dde97ea17ad27cc244c3e35a75bdf6
David Smith <codeactual@gmail.com>
2012-04-10 10:02:08 +0000

    Add all-test runner.

---
+++ test/all.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require(__dirname + '/../app/modules/mainevent');
+
+var spawn = require('child_process').spawn,
+    fs = require('fs'),
+    path = require('path'),
+    dirs = [
+      __dirname,
+      __dirname + '/jobs'
+    ],
+    parserDir = __dirname + '/../app/parsers';
+
+// Collect all parser-specific test dirs.
+_.each(fs.readdirSync(parserDir), function(dir) {
+  if (fs.statSync(parserDir + '/' + dir).isDirectory()) {
+    dirs.push(__dirname + '/../app/parsers/' + dir  + '/test');
+  }
+});
+
+// Collect all JS files from 'dirs'.
+var files = [];
+_.each(dirs, function(dir) {
+  _.each(fs.readdirSync(dir), function(file) {
+    if (path.extname(file) != '.js') {
+      return;
+    }
+    files.push(dir + '/' + file);
+  });
+});
+
+// Run each JS test file sequentially.
+mainevent.shared.Async.runSync(
+  files,
+  function(file, onFileDone) {
+    if (__filename == file) {
+      onFileDone();
+      return;
+    }
+
+    // `nodeunit` requires paths relative the CWD.
+    var relPath = path.relative(process.cwd(), file),
+        nodeunit = spawn('/usr/bin/env', ['nodeunit', relPath], {env: process.env});
+
+    nodeunit.stdout.on('data', function (data) {
+      process.stdout.write(data.toString());
+    });
+    nodeunit.stderr.on('data', function (data) {
+      process.stdout.write(data.toString());
+    });
+
+    nodeunit.on('exit', function () {
+      onFileDone();
+    });
+  }
+);
