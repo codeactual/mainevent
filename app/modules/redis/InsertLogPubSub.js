97a178995f3c5bc89da4fb6a90bf53b39c81156a
David Smith <codeactual@gmail.com>
2013-04-03 22:23:51 -0700

    Fix attempt: stop creating new clients for each message

--- app/modules/redis/InsertLogPubSub.js
+++ app/modules/redis/InsertLogPubSub.js
@@ -1,9 +1,8 @@
 'use strict';
 
+var redis = mainevent.requireModule('redis').createInstance();
+redis.connect();
+
 exports.on = function(logs) {
-  var redis = mainevent.requireModule('redis').createInstance();
-  redis.connect();
-  redis.client.publish('InsertLog', JSON.stringify(logs), function() {
-    redis.client.end();
-  });
+  redis.client.publish('InsertLog', JSON.stringify(logs));
 };
